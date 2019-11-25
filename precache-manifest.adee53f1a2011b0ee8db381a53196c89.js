c135aaa581e3544f79c4f1a772ac3c7e1ccd02d4
Serge <serge.bayet1975@gmail.com>
2019-11-25 14:04:55 +0100

    Updates

---
+++ precache-manifest.adee53f1a2011b0ee8db381a53196c89.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7021f1276ed71f6cd807ce432fcfd147",
+    "url": "/pomodoro/index.html"
+  },
+  {
+    "revision": "166dafe00f435f2f8082",
+    "url": "/pomodoro/static/css/main.c58d96a6.chunk.css"
+  },
+  {
+    "revision": "7cc7025b7f5070380006",
+    "url": "/pomodoro/static/js/2.4622d5ec.chunk.js"
+  },
+  {
+    "revision": "166dafe00f435f2f8082",
+    "url": "/pomodoro/static/js/main.9f50ae1a.chunk.js"
+  },
+  {
+    "revision": "1660f1d96f94069b7c8b",
+    "url": "/pomodoro/static/js/runtime-main.b6fa8493.js"
+  }
+]);
