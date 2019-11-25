bcdaed368fc496d7597168b062afdc9d9a55d2ab
SergeBayet <serge.bayet1975@gmail.com>
2019-11-25 07:15:38 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <PomodoroTimer />
+        <Footer />
+      </div>
+    );
+  }
+}
+
+class PomodoroTimer extends React.Component {
+  render() {
+    return (
+      <div className="pomodoro-timer">
+        <div className="time-length-control">
+          <TimerLengthControl
+            id="break-label"
+            title="Break length"
+            decrement="break-decrement"
+            increment="break-increment"
+            length="break-length"
+          />
+          <TimerLengthControl
+            id="session-label"
+            title="Session length"
+            decrement="session-decrement"
+            increment="session-increment"
+            length="session-length"
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+class TimerLengthControl extends React.Component {
+  render() {
+    return (
+      <div id={this.props.id}>
+        <h2>{this.props.title}</h2>
+        <div className="controls">
+          <span id={this.props.decrement}>
+            <i className="fa fa-arrow-down"></i>
+          </span>
+          <span id={this.props.length}>05:00</span>
+          <span id={this.props.increment}>
+            <i className="fa fa-arrow-up"></i>
+          </span>
+        </div>
+      </div>
+    );
+  }
+}
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <div className="title">
+          <h1>Pomodoro Timer</h1>
+        </div>
+        <div className="society">
+          <h1>BeCode</h1>
+        </div>
       </header>
-    </div>
-  );
+    );
+  }
+}
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer>
+        <div className="copyright">Copyright 2019. Serge Bayet</div>
+      </footer>
+    );
+  }
 }
 
 export default App;
